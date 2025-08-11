66a15cf17b2d98fdcaa54bf1f0d18ec4c9e84a5c
Kenneth Yang <microbob@is88.com>
2025-08-11 09:51:22 -0700

    Pull all assets

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,8 @@
-import { defineCollection } from "astro:content";
-import { cldAssetsLoader } from "astro-cloudinary/loaders";
+import { defineCollection } from 'astro:content';
+import { cldAssetsLoader } from 'astro-cloudinary/loaders';
 
 export const collections = {
   assets: defineCollection({
-    loader: cldAssetsLoader({
-      limit: 1000,
-      folder: 'Marymoor First Spin'
-    })
-  })
+    loader: cldAssetsLoader({limit:1024})
+  }),
 }
