6150408f6211a53f68d3d9f2575ee1694a95f94e
Kenneth Yang <kjy@is88.com>
2025-08-11 22:37:31 -0700

    Switch to prettier only

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,8 @@
-import { defineCollection } from 'astro:content';
-import { cldAssetsLoader } from 'astro-cloudinary/loaders';
+import { defineCollection } from "astro:content";
+import { cldAssetsLoader } from "astro-cloudinary/loaders";
 
 export const collections = {
   assets: defineCollection({
-    loader: cldAssetsLoader({limit:1024})
+    loader: cldAssetsLoader({ limit: 1024 }),
   }),
-}
+};
